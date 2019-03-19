dd25deb97a833a30d2857d37e25be6d44e2d7a84
E1emN <e1eman1337@gmail.com>
2019-03-19 20:10:26 +0600

    added header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,10 @@
+const styles = theme => ({
+    title: {
+        color: 'red',
+    },
+    headerContainer: {
+        justifyContent: 'space-between',
+    }
+})
+
+export default styles
--- src/root.js
+++ src/root.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/Header";
 
-function Index() {
-  return <h2>Home</h2>;
-}
+
+//function Index() {
+//  return <h2>Home</h2>;
+//}
 
 function About() {
   return <h2>About</h2>;
@@ -13,10 +15,11 @@ function Users() {
   return <h2>Users</h2>;
 }
 
+
 function AppRouter() {
   return (
     <BrowserRouter>
-        <Route path="/" exact component={Index} />
+        <Route path="/" exact component={Header} />
         <Route path="/about/" component={About} />
         <Route path="/users/" component={Users} />
     </BrowserRouter>
